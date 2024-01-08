bbfe804ffdc1ea786ed123b034bcf3aeb1229d36
shammintithi <tanhashammintithi@gmail.com>
2024-01-09 02:07:41 +0600

    cps-academy

---
+++ STL/map/1.frequency.cpp
@@ -0,0 +1,56 @@
+//way-1
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     map<string, int> id;
+
+//     id["shammin"] = 23;
+//     id["tithi"] = 62;
+//     id["raifa"] = 34;
+//     id["lamisha"] = 56;
+
+//     cout << id["tithi"] << endl;
+// }
+
+
+//way - 2
+
+//#include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     map<string, string> gender;
+
+//     gender["tithi"] = "female";
+//     gender["raifa"] = "female";
+//     gender["samir"] = "male";
+
+//     cout << gender["tithi"] << " " << gender["raifa"] << " " << gender["samir"] << endl;
+// }
+
+// //way-3
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// //string initialization = 0;
+
+// int main() {
+//     map<string, int> gender;
+
+//     cout << gender["tithi"] << endl;
+// }
+
+//way - 4
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<long long> v = {1, 19204490958580958309, 24857458475, 8356834658935739, 19204490958580958309}
+    map<long long, int> cnt;
+    
+}
+//map<index/key, value> index name;
